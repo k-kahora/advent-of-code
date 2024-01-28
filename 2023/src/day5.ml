8bdebba4780d9eabbe215a42c71803ae51d9edc0
k-kahora <mango22mjk@gmail.com>
2024-01-28 19:02:30 +0000

    Day4 part1 done

--- 2023/src/day5.ml
+++ 2023/src/day5.ml
@@ -5,40 +5,6 @@ module Ang = Angstrom
 module Lex = Lexer;;
 
 let input = Advent.read_file_as_string "2023/inputs/input5"
-let test_input = "seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4
-"
 
 type 'a state =
 | Processed of 'a
@@ -57,16 +23,12 @@ in
 rev Finish pipe
 
 
-
 type mapping_scheme = {
-start : int; finish : int; dest : int
+    start : int; finish : int; dest : int
 }
 
 let print_map map' = printf "start: %d, finish: %d, dest: %d" map'.start map'.finish map'.dest
 
-(* let digit = take_while1 Lexer.is_digit *)
-(* let whitespace = take_while Lexer.is_whitespace *)
-(* let newline = take_till Lexer.newline_char *> char '\n' *)
 let one_newline = char '\n' >>= fun _ -> peek_char_fail >>= fun a -> if Lex.is_digit a then return () else fail "epic fail"
 
 let matrix_parse = 
@@ -80,7 +42,7 @@ let seed_parse =
   let* matrix = Ang.sep_by map_seperate matrix_parse in
   return (seeds,matrix)
 
-let seeds,matrix = match parse_string ~consume:Prefix seed_parse test_input  with
+let seeds,matrix = match parse_string ~consume:Prefix seed_parse input  with
   | Ok x -> x
   | Error _ -> failwith "Big error"
 
@@ -102,15 +64,6 @@ let pipemaker acc nxt =
   Step (next,acc)
   
 let init = Step ((fun a -> a), Finish)
-let m_input = 
-[[49; 53; 8;];
-[0; 11; 42;];
-[42; 0; 7;];
-[57; 7; 4;]]
-(* NOTE Clean up all this test code here *)
-(* The code for each matrix pipeline is done  *)
-(* Final thins should involve looping throug each matrix applying the number to the pipeline *)
-(* Pass that input to the next, so a big fold *)
 let matrix_to_pipe = Core.List.map ~f:map_scheme (* test_input needs to run on everty matrix *)
 let folder = Core.List.fold ~init:init ~f:pipemaker (* folder is a pipeline *)
 (* run a fold over the whole input returning a pipeline *)
@@ -124,17 +77,25 @@ let part1 matrix' =
   
 let reset_pipe (st: int state) : int state = 
 match st with
-| Passing a -> printf "%d\n" a;Passing a
-| Processed a -> printf "%d\n" a;Passing a
+| Passing a -> Passing a
+| Processed a -> Passing a
 
 
 
 let final_pipeline = Core.List.map ~f:part1 matrix
-let result = Core.List.fold ~f:(fun state pipe -> (process_pipe state pipe) |> reset_pipe) ~init:(Passing 14) final_pipeline
+let result = Core.List.fold ~f:(fun state pipe -> (process_pipe state pipe) |> reset_pipe) final_pipeline
 
-let () = match result with 
-| Passing a -> printf "\n%d" a
-| Processed a -> printf "\n%d" a
+
+let final = Core.List.fold ~init:max_int ~f:(fun min seed -> 
+                let ext = function 
+                  | Passing a -> a
+                  | Processed a -> a
+                in
+                let location = result ~init:(Passing seed) |> ext in
+                if location < min then location else min
+              ) seeds
+
+let () = printf "Result: %d, " final
 
 
 
