bb7c36e074e6abeffb3d3154a02e89b5b63aec55
k-kahora <mango22mjk@gmail.com>
2024-01-31 19:29:45 +0000

    Part2 heavily simplified

--- 2023/src/day5.ml
+++ 2023/src/day5.ml
@@ -8,20 +8,6 @@ let input = Advent.read_file_as_string "2023/inputs/input5"
 
 let test_input = Advent.read_file_as_string "2023/inputs/test_input_day5"
 
-type 'a state = Processed of 'a | Passing of 'a
-
-type pipeline = Finish | Step of (int state -> int state) * pipeline
-
-let reverse_pipeline pipe =
-  let rec rev acc pipe' =
-    match pipe' with
-    | Step (f, rest) ->
-        rev (Step (f, acc)) rest
-    | Finish ->
-        acc
-  in
-  rev Finish pipe
-
 type mapping_scheme = {start: int; finish: int; dest: int}
 
 (* Parse the input string *)
@@ -58,42 +44,8 @@ let parse_input input' =
   | Error _ ->
       failwith "Big error"
 
-(* run a fold over the whole input returning a pipeline *)
+let seeds, matrix = parse_input input
 
-(* run through the pipeline *)
-
-let part1 matrix' =
-  let init = Step ((fun a -> a), Finish) in
-  let map_scheme = function
-    | [] ->
-        {start= -1; dest= -1; finish= -1}
-    | dest_start :: source_start :: range :: _ ->
-        {start= source_start; dest= dest_start; finish= source_start + range}
-    | _ ->
-        failwith "Input is not valid"
-  in
-  let pipemaker acc nxt =
-    let {start; dest; finish} = nxt in
-    let next a =
-      match a with
-      | Processed a ->
-          Processed a
-      | Passing a ->
-          if a <= finish && a >= start then Processed (a - start + dest)
-          else Passing a
-    in
-    Step (next, acc)
-  in
-  let nxt_pipe =
-    Core.List.map ~f:map_scheme matrix' |> Core.List.fold ~init ~f:pipemaker
-  in
-  nxt_pipe
-
-(* Run the folds neccesary *)
-let seeds, matrix = parse_input test_input
-
-(* if a <= finish && a >= start then Processed (a - start + dest) *)
-(* else Passing a *)
 let map_seed seed =
   let open Core in
   List.fold matrix ~init:seed ~f:(fun acc map ->
@@ -102,26 +54,9 @@ let map_seed seed =
       in
       match location with Some x -> acc - x.start + x.dest | None -> acc )
 
-(* let map_seed seed = *)
-(*   let reset_pipe (st : int state) : int state = *)
-(*     match st with Passing a -> Passing a | Processed a -> Passing a *)
-(*   in *)
-(*   let rec process_pipe (input : int state) (line : pipeline) = *)
-(*     match reverse_pipeline line with *)
-(*     | Step (f, rest) -> *)
-(*         process_pipe (f input) rest *)
-(*     | Finish -> *)
-(*         input *)
-(*   in *)
-(*   let process_all_pipes = *)
-(*     Core.List.fold *)
-(*       ~f:(fun state pipe -> process_pipe state pipe |> reset_pipe) *)
-(*       (Core.List.map ~f:part1 matrix) *)
-(*   in *)
-(*   let ext = function Passing a -> a | Processed a -> a in *)
-(*   process_all_pipes ~init:(Passing seed) |> ext *)
+let part1 seeds' =
+  let open Core in
+  List.fold seeds' ~init:max_int ~f:(fun mini seed ->
+      min mini @@ map_seed seed )
 
-let final sd =
-  Core.List.fold ~init:max_int ~f:(fun acc seed -> min acc @@ map_seed seed) sd
-
-let () = Format.printf "Seed: %d" @@ map_seed 55
+let () = Format.printf "Result 318728750 -> %d" @@ part1 seeds
